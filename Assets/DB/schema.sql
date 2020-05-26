a3112c337a86edc9a19ca792d8f60007115d5a1f
Alfredo Herrera <alfredherr@gmail.com>
2020-05-26 01:08:36 -0700

    pair session

--- Assets/DB/schema.sql
+++ Assets/DB/schema.sql
@@ -1,38 +1,23 @@
-DROP DATABASE if EXISTS Employee_tracker_db;
-
-create database	Employee_tracker_db; 
-
-use Employee_tracker_db; 
-
-
+DROP DATABASE if EXISTS employee_tracker_db;
+create database employee_tracker_db;
+use employee_tracker_db;
 create table department (
-	id INT not null, 
-    dept_name varchar(30), 
-    
-    primary key(id)
-); 
-
- select * from department; 
- 
+  id INT AUTO_INCREMENT NOT NULL,
+  dept_name varchar(30),
+  primary key(id)
+);
 create table roles (
-	id INT not null, 
-    title varchar(30), 
-    salary decimal(8,2),
-    department_id int, 
-    
-    primary key(id)
+  id INT AUTO_INCREMENT NOT NULL,
+  title varchar(30),
+  salary decimal(8, 2),
+  department_id int,
+  primary key(id)
 );
-
- select * from roles; 
- 
 create table employee (
-	id INT AUTO_INCREMENT NOT NULL, 
-    first_name varchar(30), 
-    last_name varchar(30), 
-    role_id INT, 
-	manager_id int not null,
-    
-    primary key(id) 
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name varchar(30),
+  last_name varchar(30),
+  role_id INT,
+  manager_id int not null,
+  primary key(id)
 );
- 
- select * from employee; 
